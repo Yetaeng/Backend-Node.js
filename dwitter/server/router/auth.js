cb76f0ba2cb70c10f51cfd8d2e0e548df8d5d5e4
yetaeng <yetaeng20@gmail.com>
2022-06-01 22:52:16 +0900

    auth 라우터 리팩토링

--- dwitter/server/router/auth.js
+++ dwitter/server/router/auth.js
@@ -1,82 +1,29 @@
 import express from 'express';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { body, param } from 'express-validator';
+import { body } from 'express-validator';
 import * as authController from '../controller/auth.js';
 import { validate } from '../middleware/validate.js';
 
 const router = express.Router();
+const credentials =
+    [   
+        body('username').trim().notEmpty().withMessage('유저이름을 입력해주세요'),
+        body('password').trim().isLength({min: 4, max: 12}).withMessage('비밀번호를 입력해주세요'),
+        validate
+    ];
 
-// 사용자 목록 저장소
-const users = [];
-const secret = 'aZqd4ywpfX*&';
+const validateSignUp = 
+    [
+        credentials,
+        body('name').notEmpty().withMessage('이름을 입력해주세요'),
+        body('email').isEmail().normalizeEmail().withMessage('이메일을 다시 입력해주세요'),
+        body('url').isURL().withMessage('url을 다시 입력해주세요').optional({nullable: true, checkFalsy: true}),
+        validate
+    ];
 
 // signup
-router.post('/signup', validate, (req, res) => {
-    const {
-        username,
-        password,
-        email,
-        url
-    } = req.body;
-    const user = users.find(user => user.username = username);
-
-    // 가입된 사용자가 아니라면 토큰 발급
-    if (user) {
-        return res.status(409).json({message: '이미 존재하는 사용자 이름 입니다.'})
-    } else {
-        const hashed = bcrypt.hashSync(password, 10);
-        const newUser = {
-            uid: users.length + 1,
-            username: username,
-            password: hashed,
-            email: email,
-            url: url
-        }
-
-        users.push(newUser);
-        const newUserToken = jwt.sign({username: username}, secret)
-
-        return res.status(201).json({message: '회원가입 성공', token: newUserToken});
-    }
-
-})
+router.post('/signup', validateSignUp, authController.register);
 
 // signin
-router.post('/signin', (req, res) => {
-    const {
-        username,
-        password
-    } = req.body;
-    const user = users.find(user => user.username = username);
-
-    // 가입되지 않은 사용자라면
-    if (!user) {
-        return res.status(404).json({message: '가입되지 않은 사용자입니다.'});
-    }
-
-    // 비번 검사
-    const compared = bcrypt.compareSync(password, user.password);
-    if (!compared) {
-        return res.status(401).json({message: '로그인 실패'})
-    }
-    
-    // 토큰 검사
-    const accessToken = req.headers.authorization.split('Bearer ')[1];
-
-    if (!accessToken) {
-        return res.status(401).json({message: "토큰이 존재하지 않습니다."})
-    } else {
-        jwt.verify(accessToken, secret, (error, decoded) => {
-            if (error) {
-                console.error(error);
-                return res.status(401).json({message: "유효하지 않은 토큰입니다."})
-            } else {
-                console.log('decoded: ', decoded);
-                return res.status(200).json({message: '로그인 성공'})
-            }
-        })
-    }
-}) 
+router.post('/signin', credentials, authController.login);
 
 export default router;
